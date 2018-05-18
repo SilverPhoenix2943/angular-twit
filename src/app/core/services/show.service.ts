ca0754f0d236fe40f76f2314c4ba8fd8ac394070
Roger Boardman <roger@bxthree.com>
2018-05-18 21:04:48 +0100

    Add service stubs

---
+++ src/app/core/services/show.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class ShowService {
+
+    constructor(private http: HttpClient) { }
+
+    getShows() { }
+
+
+}
